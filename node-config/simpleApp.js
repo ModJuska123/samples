92323f800900112c054abaed01584ca24f3dfd63
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-05-08 15:09:52 +0300

    update

--- node-config/simpleApp.js
+++ node-config/simpleApp.js
@@ -1,16 +1,16 @@
 // Importing required modules
-const express = require('express');
+// const express = require('express');
 
-// Creating an Express application
-const app = express();
+// // Creating an Express application
+// const app = express();
 
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+// // Define a route
+// app.get('/', (req, res) => {
+//   res.send('Hello, world!');
+// });
 
-// Starting the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+// // Starting the server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
